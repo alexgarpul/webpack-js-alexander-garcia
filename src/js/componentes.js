b6aabe3103026dcaf6be19b92bb81abd06f0f8c2
LozanoAndersonTheStain <adslozanov@gmail.com>
2022-08-17 17:23:41 -0500

    se termina la plantilla de webpack

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,6 +1,14 @@
-export const saludar  = (nombre)=> {
-    console.log('creando etiqueta H1')
-    const h1 = document.createElement('h1')
-    h1.innerText = `Hola, ${nombre}`
-    document.body.append(h1)
+import '../css/componentes.css'
+// import webpacklogo from '../assets/img/webpack-logo.png'
+
+export const saludar = (nombre) => {
+  console.log('Creando etiqueta h1')
+  const h1 = document.createElement('h1')
+  h1.innerText = `hola, ${nombre}`
+  document.body.append(h1)
+
+  // console.log(webpacklogo)
+  // const img = document.createElement('img')
+  // img.src = webpacklogo
+  // document.body.append(img)
 }
